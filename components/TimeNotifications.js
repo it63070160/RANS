d1f1735afe0da1aa3a6bb268bdf1583e2dac1940
it63070160 <71114745+it63070160@users.noreply.github.com>
2022-10-31 22:00:59 +0700

    Update TimeNotifications.js

--- components/TimeNotifications.js
+++ components/TimeNotifications.js
@@ -1,19 +1,16 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { Text } from 'react-native';
 
 export default function TimeNotifications(props) {
   const [count, setcount] = useState(10);
   
   useEffect(()=>{
-    const CloseTime = setTimeout(()=>{
-      props.closeModal()
-    }, 10500)
     const Intime = setInterval(()=>{
       setcount(count-1);
     }, 1000)
     if(count<=0){
       clearInterval(Intime)
-      clearTimeout(CloseTime)
+      props.autoCloseModal()
     }
     return ()=> {{clearInterval(Intime)}}
   }, [count])
@@ -22,12 +19,3 @@ export default function TimeNotifications(props) {
     <Text>{count}</Text>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-  },
-});
