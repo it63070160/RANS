679b25c4936598ff4a794c8a116ae730a46192b2
it63070160 <71114745+it63070160@users.noreply.github.com>
2022-11-18 02:29:06 +0700

    Update MainNavigator.js

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -2,13 +2,13 @@ import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { AntDesign, FontAwesome } from '@expo/vector-icons';
+import { AntDesign, FontAwesome, Feather, Ionicons, Foundation } from '@expo/vector-icons';
 // MapsView Screen
 import MapsView from '../views/MapsView';
 import NotificationsView from '../views/NotificationsView';
 import StatisticView from '../views/StatisticView';
 import ManageRisk from '../views/ManageRisk';
-import RiskStatisticView from '../views/RickStatisticView';
+import RiskStatisticView from '../views/RiskStatisticView';
 
 const Drawer = createDrawerNavigator();
 const BottomTab = createBottomTabNavigator();
@@ -29,10 +29,20 @@ function BottomTabNavigator(){
 function DrawerNavigator(){
     return (
         <Drawer.Navigator screenOptions={{ headerStyle: styles.header, headerTitleAlign: 'center'}}>
-            <Drawer.Screen name="Maps" component={MapsView} options={{title:'RANS Maps'}}/>
-            <Drawer.Screen name="ManageRisks" component={ManageRisk} options={{title:'Manage Risks'}}/>
-            <Drawer.Screen name="Notifications" component={NotificationsView}/>
-            <Drawer.Screen name="Statistic" component={BottomTabNavigator}/>
+            <Drawer.Screen name="Maps" component={MapsView} options={{
+                title:'RANS Maps',
+                drawerIcon: ({ color }) => { return <Feather name="map" size={24} color={color} />; },
+            }}/>
+            <Drawer.Screen name="ManageRisks" component={ManageRisk} options={{
+                title:'Manage Risks',
+                drawerIcon: ({color}) => { return <Foundation name="clipboard-pencil" size={30} color={color} />}
+            }}/>
+            <Drawer.Screen name="Notifications" component={NotificationsView} options={{
+                drawerIcon: ({color}) => { return <Ionicons name="notifications-outline" size={24} color={color} />}
+            }}/>
+            <Drawer.Screen name="Statistic" component={BottomTabNavigator} options={{
+                drawerIcon: ({color}) => { return <FontAwesome name="bar-chart-o" size={24} color={color} />}
+            }}/>
         </Drawer.Navigator>
     );
 }
