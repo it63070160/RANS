372bc967f2fbd66fdf36d206b677dbe38a3b68f0
it63070105 <111845618+it63070105@users.noreply.github.com>
2022-11-17 15:19:32 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
 import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-// MapsView Screen
-import MapsView from './views/MapsView';
-import NotificationsView from './views/NotificationsView';
-import StatisticView from './views/StatisticView';
-import ManageRisk from './views/ManageRisk';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+// // MapsView Screen
+// import MapsView from './views/MapsView';
+// import NotificationsView from './views/NotificationsView';
+// import StatisticView from './views/StatisticView';
+// import ManageRisk from './views/ManageRisk';
 
-const Drawer = createDrawerNavigator();
+import MainNavigator from "./navigation/MainNavigator"
+
+// const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator screenOptions={{ headerStyle: styles.header, headerTitleAlign: 'center'}}>
-        <Drawer.Screen name="Maps" component={MapsView} options={{title:'RANS Maps'}}/>
-        <Drawer.Screen name="ManageRisks" component={ManageRisk} options={{title:'Manage Risks'}}/>
-        <Drawer.Screen name="Notifications" component={NotificationsView}/>
-        <Drawer.Screen name="Statistic" component={StatisticView}/>
-      </Drawer.Navigator>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <Drawer.Navigator screenOptions={{ headerStyle: styles.header, headerTitleAlign: 'center'}}>
+    //     <Drawer.Screen name="Maps" component={MapsView} options={{title:'RANS Maps'}}/>
+    //     <Drawer.Screen name="ManageRisks" component={ManageRisk} options={{title:'Manage Risks'}}/>
+    //     <Drawer.Screen name="Notifications" component={NotificationsView}/>
+    //     <Drawer.Screen name="Statistic" component={StatisticView}/>
+    //   </Drawer.Navigator>
+    // </NavigationContainer>
+    <MainNavigator/>
   );
 }
 
